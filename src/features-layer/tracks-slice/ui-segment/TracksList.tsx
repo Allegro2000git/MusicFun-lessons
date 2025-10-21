c90a617974cf42bc921e2027cfa23b3d21f658c4
Oleg Kova <okovalchuk722@gmail.com>
2025-10-21 13:50:39 +0300

    lesson 9 sort-By,Direction

--- src/features-layer/tracks-slice/ui-segment/TracksList.tsx
+++ src/features-layer/tracks-slice/ui-segment/TracksList.tsx
@@ -3,6 +3,7 @@ import {type ChangeEvent, useEffect, useRef, useState} from "react";
 import {useTracksQuery} from "../model-segment/useTracksQuery";
 import {Pagination} from "../../../shared-layer/ui-segment/Pagination";
 import {Search} from "../../../shared-layer/ui-segment/Search";
+import {useSearchParams} from "react-router";
 
 function usePagination () {
     const [pageNumber, setPageNumber] = useState(1)
@@ -20,13 +21,17 @@ function usePagination () {
 }
 
 export function TracksList() {
+    const [searchSortParams, setSearchSortParams] = useSearchParams()
+
+    const sortBy = searchSortParams.get('sortBy')
+    const sortDirection = searchSortParams.get('sortDirection')
 
     const [currentPlayingTrack, setCurrentPlayingTrack] = useState<string | null>(null)
     const listRef = useRef<HTMLUListElement | null>(null)
     const [search, setSearch] = useState("")
     const {pageNumber, setPageNumber, pageSize, setPageSize} = usePagination()
 
-    const {data, isPending, isError} = useTracksQuery({pageNumber, pageSize, search})
+    const {data, isPending, isError} = useTracksQuery({pageNumber, pageSize, search, sortBy, sortDirection})
 
     useEffect(() => {
         function scrollToTrack(trackId: string) {
@@ -50,6 +55,20 @@ export function TracksList() {
             scrollToTrack(currentPlayingTrack);
         }
     }, [currentPlayingTrack, data?.data])
+
+    const handleSortChange = (newSortBy: string) => {
+        let newSortDirection = 'desc'
+        if (sortBy === newSortBy) {
+            newSortDirection = sortDirection === 'asc' ? 'desc' : 'asc'
+        }
+
+        setSearchSortParams(prev => {
+            const newSearchSortParams = new URLSearchParams(prev)
+            newSearchSortParams.set('sortBy', newSortBy)
+            newSearchSortParams.set('sortDirection', newSortDirection)
+            return newSearchSortParams
+        })
+    }
 
     if (isPending) {
         return <div>loading...</div>
@@ -90,7 +109,27 @@ export function TracksList() {
     return (
         <>
             <Search onSearch={handleSearchClick} isSearchButtonVisibles={false} mode={'throttle'}/>
-
+            <div>
+                <span>Сортировка: </span>
+                <button
+                    onClick={() => handleSortChange('publishedAt')}
+                    style={{
+                        fontWeight: sortBy === 'publishedAt' ? 'bold' : 'normal',
+                        margin: '0 5px'
+                    }}
+                >
+                    По дате публикации {sortBy === 'publishedAt' && (sortDirection === 'asc' ? '↑' : '↓')}
+                </button>
+                <button
+                    onClick={() => handleSortChange('likesCount')}
+                    style={{
+                        fontWeight: sortBy === 'likesCount' ? 'bold' : 'normal',
+                        margin: '0 5px'
+                    }}
+                >
+                    По лайкам {sortBy === 'likesCount' && (sortDirection === 'asc' ? '↑' : '↓')}
+                </button>
+            </div>
             <hr/>
             <select defaultValue={pageSize} onChange={handlePageSizeChange}>
                 <option value={5}>5</option>
--- src/shared-layer/ui-segment/Search.tsx
+++ src/shared-layer/ui-segment/Search.tsx
@@ -11,13 +11,34 @@ export function Search({onSearch, isSearchButtonVisibles = true, mode = 'immedia
     const timerIdRef = useRef<number>(undefined)
     const throttleIsWaitingRef = useRef<boolean>(false)
     const searchValueRef = useRef<string>('')
+    const [error, setError] = useState('')
+
+    const validateSearch = (value: string): boolean => {
+        if (value === '') {
+            setError('')
+            return true
+        }
+        const isValid = /^[a-zA-Zа-яА-ЯёЁ0-9_\s]*$/.test(value)
+
+        if (!isValid) {
+            setError('Допустимы только буквы (английские и русские), цифры, пробел и знак подчеркивания')
+            return false
+        }
+        return isValid
+    }
 
     const handleSearchClick = () => {
-        onSearch(search)
+        if (validateSearch(search)) {
+            onSearch(search)
+        }
     }
 
     useEffect(() => {
         if (!isSearchButtonVisibles) {
+            if (!validateSearch(search)) {
+                return
+            }
+
             switch (mode) {
                 case 'immediate':
                     onSearch(search)
@@ -51,14 +72,20 @@ export function Search({onSearch, isSearchButtonVisibles = true, mode = 'immedia
 
     const handleSearchChange = (e: ChangeEvent<HTMLInputElement>) => {
         const value = e.currentTarget.value
+        validateSearch(value)
         setSearch(value)
         searchValueRef.current = value
     }
 
     return (
         <>
-            <input value={search} onChange={handleSearchChange}/>
-            {isSearchButtonVisibles && <button onClick={handleSearchClick}>Search</button>}
+            <input value={search}
+                   onChange={handleSearchChange}
+                   style={{borderColor: error ? 'red' : '', backgroundColor: error ? '#fff0f0' : ''}}
+                   aria-invalid={!!error}
+                   />
+            {isSearchButtonVisibles && <button onClick={handleSearchClick} disabled={!!error && search !== ''}>Search</button>}
+            {error && <div style={{color: 'red', fontSize: '12px', marginTop: '4px'}}>{error}</div>}
         </>
 
     )
